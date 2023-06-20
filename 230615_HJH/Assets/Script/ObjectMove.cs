943361cff33aa0e6ea8d44f00d80fe4c4804088c
wlstl <105594802+wlstl@users.noreply.github.com>
2023-06-20 20:13:12 +0900

    깃허브 저장
    ㅇㅇ

---
+++ 230615_HJH/Assets/Script/ObjectMove.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectMove : MonoBehaviour{
+    int step = 0;
+    private void Update(){
+        float y = this.transform.position.y;
+        float x = this.transform.position.x;
+
+
+
+    }
+}
